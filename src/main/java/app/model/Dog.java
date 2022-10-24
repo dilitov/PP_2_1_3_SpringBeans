c03193ace8df5c5c89821140c7dad532cf85c1cb
Dilitov <tfutka@gmail.com>
2022-10-24 16:49:16 +0300

    Initial

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
